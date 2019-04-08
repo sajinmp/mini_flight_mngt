09042273d8d8fe8de5d91f37577c1841c8cf2ff7
sajinmp <sajinprasadkm@gmail.com>
2019-04-08 06:01:52 +0530

    bug fix for nil to float

--- app/controllers/concerns/booking_concerns.rb
+++ app/controllers/concerns/booking_concerns.rb
@@ -6,10 +6,10 @@ module Concerns
       if check_price(params[:booking][:amount], @old_booking&.amount)
         new_params = booking_params.except(:amount).merge!(flight_id: @pnr.flight_id, pnr_id: @pnr.id,
                               status: GlobalConfig.with_key('booked', 'booking_status'))
-        new_params.merge!(amount: params[:booking][:amount].to_f + @old_booking&.amount&.to_f)
+        new_params.merge!(amount: params[:booking][:amount].to_f + @old_booking&.amount.to_f)
         new_params.merge!(upgraded_booking_id: @old_booking.id, previous_amount: @old_booking.amount) if @old_booking
         booking = Booking.create!(new_params)
-        @old_booking.update!(status: GlobalConfig.with_key('upgrade', 'booking_status'))
+        @old_booking.update!(status: GlobalConfig.with_key('upgrade', 'booking_status')) if @old_booking
         flash[:success] = 'Seat booking success'
         redirect_to booking
       else
