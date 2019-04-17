8b6b4b604f53ec934001d136882556a27f157dd3
sajinmp <sajinprasadkm@gmail.com>
2019-04-17 18:31:01 +0530

    check whether seat is available before listing flights

--- app/controllers/concerns/booking_concerns.rb
+++ app/controllers/concerns/booking_concerns.rb
@@ -3,7 +3,40 @@ module Concerns
     def list_flights(params)
       @origin, @destination, @no_of_seats = params[:origin], params[:destination], params[:no_of_seats]
       @direct_flights = Flight.flights_with_origin_destination(@origin, @destination)
+      @direct_flights = check_seat_availability(@direct_flights, @no_of_seats)
       @connected_flights = Flight.join_flights.connected_flights(@origin, @destination).select_connected_flight_details
+      @connected_flights = check_seat_availability(@connected_flights, @no_of_seats, true)
+    end
+
+    def check_seat_availability(flights, seats, connected = nil)
+      return check_seat_in_direct_flights(flights, seats) unless connected
+      check_seat_in_connected_flights(flights, seats)
+    end
+
+    def check_seat_in_connected_flights(flights, seats)
+      flight_ids = Array.new << flights.ids << flights.map(&:connection_id)
+      impossible_flight_ids = get_seat_counts(flight_ids.flatten, seats)
+      return flights if impossible_flight_ids.blank?
+      flights.where('flights.id not in (?) and b.id not in (?)', impossible_flight_ids, impossible_flight_ids)
+    end
+
+    def check_seat_in_direct_flights(flights, seats)
+      impossible_flight_ids = get_seat_counts(flights.ids, seats)
+      flights.where.not(id: impossible_flight_ids)
+    end
+
+    def get_seat_counts(flight_ids, seats)
+      booked_count = Hash[Booking.joins(:passenger_details).where(status: GlobalConfig.with_key('booked', 'booking_status'),
+          passenger_details: {flight_id: flight_ids}).select('passenger_details.flight_id as flight_id,
+          count(passenger_details.id) as booked_seats').group('passenger_details.flight_id')
+          .map { |i| [i.flight_id, i.booked_seats] }]
+      max_count = Flight.joins(:seat_configs).where(id: flight_ids).select('flights.id as id, sum(seat_configs.no_of_rows*seat_configs.seats_in_row) as max_seats').group('flights.id').map { |i| [i.id, i.max_seats] }
+      remaining_seats = {}
+      max_count.each do |flight_id, max_seats|
+        remaining_seats[flight_id] = max_seats - booked_count[flight_id].to_i
+      end
+      impossible_flight_ids = remaining_seats.collect { |k, v| k if v < seats.to_i }.compact
+      impossible_flight_ids
     end
 
     def create_booking(params)
